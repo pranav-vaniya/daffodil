c6313b7064ca4e0b27e544b7f51ac4ce5fca9337
Pranav Vaniya <pmvaniya16@gmail.com>
2025-10-19 14:35:46 +0530

    Service worker

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+if ("serviceWorker" in navigator) {
+	navigator.serviceWorker.register("./sw.js");
+}
+
 window.addEventListener("resize", () => {
 	let vh = window.innerHeight * 0.01;
 	document.documentElement.style.setProperty("--vh", `${vh}px`);
