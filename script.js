f0285cdacc0fc6f7782dc027a045426b4bcdffc7
Pranav Vaniya <pmvaniya16@gmail.com>
2025-10-20 09:28:38 +0530

    color theme

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 if ("serviceWorker" in navigator) {
 	navigator.serviceWorker.register("./sw.js");
 }
-
-window.addEventListener("resize", () => {
-	let vh = window.innerHeight * 0.01;
-	document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
